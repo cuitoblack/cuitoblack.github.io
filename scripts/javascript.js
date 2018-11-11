1f7b17b32ed6a910e331c04da213328d067e4d8e
Cuito Naude <cuitonaude@gmail.com>
2018-11-11 22:10:29 +0200

    summarytied

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -11,3 +11,21 @@ function closeForm() {
 function sendEmail() {
   document.getElementById("successMessage").style.visibility = "visible";
 }
+
+function chooseKak() {
+  document.getElementById("lekker").style.background-color = "white";
+  document.getElementById("kaklekker").style.background-color = "white";
+  document.getElementById("kak").style.background-color = "black";
+}
+
+function chooseLekker() {
+  document.getElementById("kak").style.background-color = "white";
+  document.getElementById("kaklekker").style.background-color = "white";
+  document.getElementById("lekker").style.background-color = "black";
+}
+
+function chooseKakLekker() {
+  document.getElementById("lekker").style.background-color = "white";
+  document.getElementById("kak").style.background-color = "white";
+  document.getElementById("kaklekker").style.background-color = "black";
+}
